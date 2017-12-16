dd718c21676e82cdb0696b25da61f6e8a339d808
thavoo <gustavoh.2312@gmail.com>
2017-12-15 22:53:14 -0400

    update

---
+++ backend/app/liquens.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class liquens extends Model
+{
+    //
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('/liquen','Liquends', ['middleware' => 'auth:api']);
